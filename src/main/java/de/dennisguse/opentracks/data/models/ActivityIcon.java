43859cfcd2fea563b2ac1491f755edde8c733024
katrodiyaarchil <katrodiyaarchil@gmail.com>
2024-02-14 22:17:42 -0500

    Added private constructor to hide the implicit public one

--- src/main/java/de/dennisguse/opentracks/data/models/ActivityIcon.java
+++ src/main/java/de/dennisguse/opentracks/data/models/ActivityIcon.java
@@ -5,5 +5,7 @@ import de.dennisguse.opentracks.R;
 public class ActivityIcon {
 
     static final int ICON_UNKNOWN = R.drawable.ic_logo_24dp;
-
+    private ActivityIcon(){
+        // Private constructor to prevent instantiation of the class
+    };
 }
