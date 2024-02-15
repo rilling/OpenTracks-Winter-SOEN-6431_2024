8f7ae53778672d19d10b8dc90f405d7c34aabb01
Abdul Hai Anik <58440210+Anik131@users.noreply.github.com>
2024-02-15 01:34:05 -0500

    #7 Refactor ShareUtils to Utilize Custom Exceptions

---
+++ src/main/java/de/dennisguse/opentracks/share/ShareUtilsException.java
@@ -0,0 +1,7 @@
+package de.dennisguse.opentracks.share;
+
+public class ShareUtilsException extends RuntimeException {
+    public ShareUtilsException(String message) {
+        super(message);
+    }
+}
