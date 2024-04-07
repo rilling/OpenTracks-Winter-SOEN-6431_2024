280a89c0a3858af795b7d9a424e0861cc42f096c
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-04-07 18:14:26 -0400

    Add placeholder logic for number of maintenance activities.

--- src/main/java/de/dennisguse/opentracks/settings/SkiProfileStatisticsFragment.java
+++ src/main/java/de/dennisguse/opentracks/settings/SkiProfileStatisticsFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.time.Duration;
+
 import de.dennisguse.opentracks.R;
 
 public class SkiProfileStatisticsFragment extends PreferenceFragmentCompat {
@@ -16,6 +18,25 @@ public class SkiProfileStatisticsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.fragment_ski_profile_statistics);
+
+        int totalKmSkied = 50; // THIS IS DEPENDANT ON THE WORK OF OTHER GROUP
+        int intervalOfWaxing = 10; //THIS HAS BEEN DONE BY OUR GROUP. WILL IMPLEMENT HERE ONCE WE HAVE TOTAL KM OF SKI.
+        int intervaleOfSharpening = 5; //THIS HAS BEEN DONE BY OUR GROUP. WILL IMPLEMENT HERE ONCE WE HAVE TOTAL KM OF SKI.
+        int numOfWaxing = totalKmSkied / intervalOfWaxing;
+        int numOfSharpening = totalKmSkied / intervaleOfSharpening;
+        findPreference("waxing")
+                .setSummaryProvider(
+                        preference -> {
+                            return numOfWaxing + " Waxing activities performed.";
+                        }
+                );
+
+        findPreference("sharpening")
+                .setSummaryProvider(
+                        preference -> {
+                            return numOfSharpening + " Sharpening activities performed.";
+                        }
+                );
     }
 
 }
